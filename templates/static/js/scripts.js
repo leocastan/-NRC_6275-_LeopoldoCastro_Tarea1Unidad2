a1767bea6c96b80046a79440258400c34f68a27b
lecastro3 <lecastro3@espe.edu.ec>
2022-07-19 23:44:52 -0500

    5th commit

--- templates/static/js/scripts.js
+++ templates/static/js/scripts.js
@@ -1,10 +1,9 @@
 (function () {
   'use strict'
 
-  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+  // Get the DOM where we want to apply Bootstrap validation
   var forms = document.querySelectorAll('.needs-validation')
 
-  //Recorremos los forms y evitamos el envío sin validacion
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
